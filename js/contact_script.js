a1bcdd3808ca9bc215968adf2a82248074cba6a0
DeegantoBanerjee <50057038+DeegantoBanerjee@users.noreply.github.com>
2019-08-04 23:13:22 +0530

    final-3

--- js/contact_script.js
+++ js/contact_script.js
@@ -1,15 +1,21 @@
 
 function show_menu(){ 
+	document.body.scrollTop = 0;
 	var x = document.getElementById("menu");
-    var y = document.getElementById("particles-js");
-    var b = document.getElementById("top");
-    b.classList.toggle("change");
+	var y = document.getElementById("particles-js");
+	var z = document.getElementById("top");
+	z.classList.toggle("change");
 	if (x.style.display == "block") {
     x.style.display = "none";
 	y.style.transform ="scale(1) translateY(0px)";
+    z.style.overflowY = "auto";
+    y.style.overflowY = "auto";
     } 
     else {
     x.style.display = "block";
 	y.style.transform ="scale(.9) translateY(200px)";
-    }   	
+	z.style.overflowY = "hidden";	
+    y.style.overflowY = "hidden";	
+
+}   	
 }
